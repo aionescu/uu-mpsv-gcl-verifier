8ff3ccae799f876e9dd131f242dfc219c5b8fc66
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2022-09-25 16:09:22 +0200

    Finished pattern

--- src/Language/GCL/Verification.hs
+++
@@ -1,8 +0,0 @@
-module Language.GCL.Verification where
-import Language.GCL.Syntax
-import Language.GCL.Verification.Preprocessing(preprocess)
-import Language.GCL.Verification.Wlp(runWLP)
-
-verify :: Program -> Pred
-verify = runWLP . preprocess
-
---
+++ src/Language/GCL/Verify.hs
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+module Language.GCL.Verification where
+import Language.GCL.Syntax
+import Language.GCL.Verification.Preprocessing(preprocess)
+import Language.GCL.Verification.Wlp(runWLP)
+
+
+verify :: Program -> Pred
+verify = runWLP . preprocess
