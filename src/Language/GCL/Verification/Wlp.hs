01e5e70750c234e3526eb60fe52332949a926918
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2022-10-02 18:34:10 +0200

    new arrays, added test case

--- src/Language/GCL/Verification/Wlp.hs
+++ src/Language/GCL/Verification/Wlp.hs
@@ -2,8 +2,9 @@ module Language.GCL.Verification.Wlp(runWlp) where
 
 import Language.GCL.Syntax
 import Data.Functor.Foldable(para)
-import Language.GCL.Syntax.Helpers (pattern T, (∧), (⟹), (¬) )
+import Language.GCL.Syntax.Helpers (pattern T, (∧), (⟹), (¬), ifEqExpr )
 import Data.Fix(Fix(..))
+import Data.Functor.Foldable
 
 subst :: Id -> Expr -> Pred -> Pred
 subst i e = para \case
@@ -13,6 +14,16 @@ subst i e = para \case
   Exists v (p, _) | i == v -> Fix $ Exists v p
   p -> Fix $ snd <$> p
 
+substSubscript :: Id -> Expr -> Expr -> Pred -> Pred
+substSubscript id c e' = cata \case 
+  Subscript i e | i == id -> ifEqExpr e c e' $ Fix (Subscript i e)
+  z -> Fix z
+
+-- paratest :: Id -> Expr -> Pred -> Pred
+-- paratest i e = para \case
+--   (d, b) -> d
+--   p -> Fix $ snd <$> p
+
 wlp :: StmtF Stmt -> Pred -> Pred
 wlp Skip q = q
 wlp (Assign i e) q = subst i e q
@@ -20,6 +31,7 @@ wlp (Seq s₁ s₂) q = wlp (unFix s₁) $ wlp (unFix s₂) q
 wlp (Assert e) q = e ∧ q
 wlp (Assume e) q = e  ⟹ q
 wlp (If g s₁ s₂) q = (g ⟹ wlp (unFix s₁) q) ∧ ((¬)g ⟹ wlp (unFix s₂) q)
+wlp (AssignIndex ad i as) q = substSubscript ad i as q
 wlp s _ = error $ "WLP not yet implemented for: " <> show (Fix s)
 
 runWlp :: Program -> Pred
